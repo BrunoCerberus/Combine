aaa58b5ad6b23c4c302572347d07653332d54fa0
Bruno Lopes <bruno.guitarpro@gmail.com>
2023-07-27 17:05:39 -0300

    Add a URLSession publisher tutorial

---
+++ Network.playground/Contents.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+import Combine
+import Foundation
+
+struct MobileResponse: Codable, Equatable {
+    let movies: [Movie]
+        struct Movie: Codable, Equatable {
+            let title: String
+            let releaseYear: Int
+            
+            enum CodingKeys: CodingKey {
+                case title
+                case releaseYear
+            }
+        }
+}
+
+func fetchMovies() -> AnyPublisher<MobileResponse, Error> {
+    let url = URL(string: "https://api.themoviedb.org/3/movie/upcoming?api_key=\(apiKey)")!)!
+    
+    return URLSession
+        .shared
+        .dataTaskPublisher(for: url)
+        .map(\.data)
+        .decode(type: MobileResponse.self, decoder: JSONDecoder())
+        .eraseToAnyPublisher()
+}
+
+final class MovieViewModel: ObservableObject {
+    @Published var movies: [MobileResponse.Movie] = []
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    func fetchData() {
+        fetchMovies()
+            .map(\.movies)
+            .receive(on: DispatchQueue.main)
+            .replaceError(with: [])
+            .print()
+            .assign(to: \.movies, on: self)
+            .store(in: &cancellables)
+    }
+}
+
+let vm = MovieViewModel()
+vm.fetchData()
